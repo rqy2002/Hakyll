f8fc07009751f6f05cd83ece2c61d90111f7587f
rqy2002 <1458814497@qq.com>
2022-11-24 23:04:53 +0800

    quq

--- js/proof-collapse.js
+++ js/proof-collapse.js
@@ -3,9 +3,10 @@ $(document).ready(function () {
     // Proof expander
     if ($('.proof-collapsible:not(.initialized)').length > 0) {
       $('.proof-collapsible').addClass('initialized');
-      $('.proof-expander.proof-expander-expanding').html('<svg height="16" width="16"><path stroke="#999" stroke-width="1.5" fill="none" d="M8 2 L14 8 L8 14"></path></svg> ');
-      $('.proof-expander.proof-expander-collapsing').html('<svg height="16" width="16"><path stroke="#999" stroke-width="1.5" fill="none" d="M2 6 L8 12 L14 6"></path></svg> ');
+      $('.proof-expander.proof-expander-expanding').html('<a><svg height="16" width="16"><path stroke="#999" stroke-width="1.5" fill="none" d="M8 2 L14 8 L8 14"></path></svg></a>');
+      $('.proof-expander.proof-expander-collapsing').html('<a><svg height="16" width="16"><path stroke="#999" stroke-width="1.5" fill="none" d="M2 6 L8 12 L14 6"></path></svg></a>');
       $('.proof-expander.proof-expander-ellipsis').html('<span style="font-size:40%">• • •</span>');
+      $('.proof-header').append('<div class="floatright"><p><span class="inline-math"><span class="katex"><span class="katex-html" aria-hidden="true"><span class="base"><span class="strut" style="height: 0.675em;"></span><span class="mord amsrm">□</span></span></span></span></span></p></div>');
       $('.proof-expander').click(function () {
         let $this = $(this);
         let $parent = $this.closest('.proof-collapsible');
